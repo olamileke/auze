899fc08397d0768e2257580f42b096d0e2fe25cf
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-08-04 00:02:30 +0100

    feat: updated home page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="fixed top-[6.5vh] left-[8vh]">
+    <p class="text-lg underline decoration-double">Auze</p>
+  </div>
   <RouterView />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,10 @@
 
 <template>
     <section class="w-screen min-h-screen bg-brand-100/20 flex justify-center items-center">
-        <div class="flex gap-4 relative -top-[5vh]">
-            <div v-for="(api, index) in apis" :key="index" class="p-5 flex border border-black border-dashed rounded-sm cursor-pointer">
+        <div class="flex gap-4 relative -top-[8vh]">
+            <div v-for="(api, index) in apis" :key="index" class="p-5 flex border-2 border-black hover:border-2 rounded-sm cursor-pointer">
                 <div class="w-16 h-16 bg-black text-white text-xl mr-3 flex justify-center items-center text-lg">{{ api.charAt(0) }}</div>
-                <p>{{ api }} API Example</p>
+                <p class="underline decoration-double">{{ api }} API Example</p>
             </div>
         </div>
     </section>
