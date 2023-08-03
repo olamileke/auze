e94c64a528b9a6ff0a5fc6ace7c85b4d4f9165d0
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-08-03 23:43:13 +0100

    chore: added .gitattributes to ignore css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 <template>
     <section class="w-screen min-h-screen bg-brand-100/20 flex justify-center items-center">
         <div class="flex gap-4 relative -top-[5vh]">
-            <div v-for="(api, index) in apis" :key="index" class="p-5 flex border border-black border-dashed rounded-sm">
+            <div v-for="(api, index) in apis" :key="index" class="p-5 flex border border-black border-dashed rounded-sm cursor-pointer">
                 <div class="w-16 h-16 bg-black text-white text-xl mr-3 flex justify-center items-center text-lg">{{ api.charAt(0) }}</div>
                 <p>{{ api }} API Example</p>
             </div>
